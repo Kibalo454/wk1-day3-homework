d408354249a727a721ef67538a6d06200cc20514
Kibalo454 <k.passigue.9247@westcliff.edu>
2025-11-08 22:56:44 -0500

    Completed Wk1 Day3: FoodBlog Pug Components

---
+++ foodblog-components/server.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (_req, res) => res.render('pages/foodblog'));
+
+app.listen(3000, () => console.log('http://localhost:3000'));
